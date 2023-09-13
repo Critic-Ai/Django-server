12add8bfb0f306ce1c2d3b1cf504d8bd1190cf57
Soham Datta <sohamdatta34@gmail.com>
2023-09-13 21:41:53 +0530

    fix: added error handling and home router

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_records
+from .views import home, get_records
 
 urlpatterns = [
+    path('', home, name='home'),
     path('api/records/', get_records, name='get_records'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,30 +1,48 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 load_dotenv(find_dotenv())
 
 URI = os.environ['DB_URI']
+DB_NAME = os.environ['DB_NAME']
+COLL_NAME = os.environ['COLL_NAME']
 
 client = MongoClient(URI, server_api=ServerApi('1'))
 
 
+def home(request):
+    content = '<p>There are thirty million songs on Spotify</p>' \
+        '<p>Thirty million songs</p>' \
+        '<p>Some are real big hits</p>' \
+        '<p>Some are really... not</p>' \
+        '<p>All songs are not created equal</p>' \
+        '<p>Play the songs you love</p>' \
+        '<p>Skip the ones you don\'t</p>' \
+        '<p>Get a free thirty day trial of Premium</p>' \
+        '<p>Skip whatever, we won\'t take it personally</p>' \
+        '<p>So try premium, and start skipping</p>' \
+        '<p>Tap the banner now to find out more</p>'
+
+    return HttpResponse(content)
+
+
 def get_records(request):
-    # return JsonResponse({'url': URI}) # works
+    try:
+        db = client[DB_NAME]
+        collection = db[COLL_NAME]
+        records = collection.find()
 
-    dbName = "Ratings"
-    RatingCollections = "RatingCollections"
+        record_list = []
 
-    db = client[dbName]
-    mycollection = db[RatingCollections]
-    records = mycollection.find()
+        for game in records:
+            game['_id'] = str(game['_id'])
+            record_list.append(game)
 
-    record_list = []
-    for record in records:
-        record['_id'] = str(record['_id'])  # Convert ObjectId to string
-        record_list.append(record)
+        return JsonResponse({'data': record_list})
 
-    return JsonResponse({'data': record_list})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
